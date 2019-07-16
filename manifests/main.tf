fdd35ff19961102d358428341a756245eb56b076
Arcaelyx <arcaelyx@protonmail.com>
2019-07-16 15:22:19 -0400

    Provide manifest file for droplet deployments
    In order to leverage Terraform for the deployment of droplets, a
    manifest file has been provided with a basic configuration for the
    deployment of a small droplet in the NYC region.
    
    This will need to be followed up with the provisioning of Docker on said
    deployment so as to execute the release image to finalize the deployment
    process. This may replace DigitalOcean with AWS for subsequent
    deployments.

---
+++ manifests/main.tf
@@ -0,0 +1,23 @@
+variable "token" {}
+variable "ssh_key_id" {}
+
+provider "digitalocean" {
+  token = "${var.token}"
+}
+
+resource "digitalocean_droplet" "haven-fe" {
+  ssh_keys = ["${var.ssh_key_id}"]
+  image    = "debian-9-x64"
+  region   = "nyc3"
+  size     = "512mb"
+  name     = "haven-fe"
+
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      private_key = "${file("~/.ssh/id_rsa")}"
+      user        = "root"
+      timeout     = "2m"
+    }
+  }
+}
