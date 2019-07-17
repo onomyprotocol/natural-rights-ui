b9aa0700c8c9732de617ada6d858a02dafeca544
Arcaelyx <arcaelyx@protonmail.com>
2019-07-17 13:37:44 -0400

    Provide provisioning & execution of containers on deployment targets
    In order to leverage the container tagged for release, docker is now
    installed on the newly created droplet where it pulls the release image
    from GitLab's registry and executes it.

--- manifests/main.tf
+++ manifests/main.tf
@@ -17,6 +17,18 @@ resource "digitalocean_droplet" "haven-fe" {
   name     = "haven-fe"
 
   provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common",
+      "curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -",
+      "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable\"",
+      "sudo apt update",
+      "sudo apt install -y docker-ce docker-ce-cli containerd.io",
+      "docker login -u ${var.username} -p ${var.password} ${var.registry}",
+      "docker pull ${var.image}",
+      "docker run -d -p 80:80 ${var.image}"
+    ]
+
     connection {
       type        = "ssh"
       host        = "${digitalocean_droplet.haven-fe.ipv4_address}"
